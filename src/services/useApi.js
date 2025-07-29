e4a95877aac798080bacf32b158fcab5f2276977
TheElite44 <eliterozariho@gmail.com>
2025-07-29 23:09:16 +0500

    Update useApi.js

--- src/services/useApi.js
+++ src/services/useApi.js
@@ -4,7 +4,7 @@ import config from "../config/config";
 
 export const API_BASE_URL =
   import.meta.env.VITE_APP_MODE &&
-  import.meta.env.VITE_APP_MODE === "development"
+  import.meta.env.VITE_APP_MODE === "production"
     ? config.localUrl
     : config.serverUrl;
 
